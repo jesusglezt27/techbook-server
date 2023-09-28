54d387460f8deb0349138ab4bf1647b2b1804bad
Jesus Gonzalez <jesusglezt27@gmail.com>
2023-09-27 18:12:42 -0600

    doness

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/techbooks";
+  process.env.MONGODB_URI || "mongodb+srv://jesusglezt27:pCcovhjKlg73Ygn5@techbooksx.ppnm4qu.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
